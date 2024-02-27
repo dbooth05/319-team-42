43d8f67c2dbcbaa032229b1a0c149130bed8764e
Dylan <dbooth05@iastate.edu>
2024-02-27 13:50:23 -0600

    implemented base of searching for title

--- Midterm_42/index.js
+++ Midterm_42/index.js
@@ -25,20 +25,43 @@ function loadAll(movies) {
 
 }
 
-function loadSearch() {
+function loadFilter() {
+
+}
+
+function getLoadSearch() {
 
     fetch("./data.json")
         .then(response => response.json())
-        .then(movies => loadAll(movies));
+        .then(movies => loadSearch(movies));
 
-    var container = document.getElementById("movie_list");
+    function loadSearch(movies) {
 
-    while (container.lastElementChild) {
-        container.removeChild(container.lastElementChild);
-    }
+        var container = document.getElementById("movie_list");
 
-    for (var i = 0; i < movies.movies.length; i++) {
+        let searchName = document.forms["search_form"]["search"];
+        let inputSearchName = searchName.value;
+
+        while (container.lastElementChild) {
+            container.removeChild(container.lastElementChild);
+        }
+    
+        for (var i = 0; i < movies.movies.length; i++) {
+            let title = movies.movies[i].title;
+            console.log(title.includes(inputSearchName));
+            if (title.includes(inputSearchName)) {
+                let title = movies.movies[i].title;
+                let desc = movies.movies[i].description;
         
+                let div = document.createElement("div");
+                div.innerHTML = `
+                    <h3>${title}<\h3><br>
+                    <h3>${desc}<\h3>
+                `;
+        
+                container.append(div);
+            }
+        }
     }
 
 }
