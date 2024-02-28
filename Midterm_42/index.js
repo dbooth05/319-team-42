d0f40960a65d8b6ba18795c4b4f9aada67f675a5
Dylan <dbooth05@iastate.edu>
2024-02-27 22:34:59 -0600

    added genre fliter function to script

--- Midterm_42/index.js
+++ Midterm_42/index.js
@@ -1,31 +1,102 @@
-fetch("./data.json")
-.then(response => response.json())
-.then(movies => loadAll(movies));
+window.onload = getLoadAll;
 
-function loadAll(movies) {
-    
-    var container = document.getElementById("movie_list");
+function getLoadAll() {
 
-    while (container.lastElementChild) {
-        container.remove(container.lastElementChild);
+    fetch("./data.json")
+    .then(response => response.json())
+    .then(movies => loadAll(movies));
+
+    function loadAll(movies) {
+        
+        var container = document.getElementById("movie_list");
+
+        console.log("made it here");
+
+        while (container.lastElementChild) {
+            container.remove(container.lastElementChild);
+        }
+
+        for (var i = 0; i < movies.movies.length; i++) {
+            let title = movies.movies[i].title;
+            let desc = movies.movies[i].description;
+
+            let div = document.createElement("div");
+            div.innerHTML = `
+                <h3>${title}<\h3><br>
+                <p>${desc}<\p>
+            `;
+
+            container.append(div);
+        }
+
     }
-
-    for (var i = 0; i < movies.movies.length; i++) {
-        let title = movies.movies[i].title;
-        let desc = movies.movies[i].description;
-
-        let div = document.createElement("div");
-        div.innerHTML = `
-            <h3>${title}<\h3><br>
-            <p>${desc}<\p>
-        `;
-
-        container.append(div);
-    }
-
 }
 
-function loadFilter() {
+function getLoadFilter() {
+
+    fetch("./data.json")
+        .then(response => response.json())
+        .then(movies => loadFilter(movies));
+
+    function loadFilter(movies) {
+
+        var container = document.getElementById("movie_list");
+
+        var checkboxes = document.querySelectorAll('#genres input[type="checkbox"]');
+        var selectGenres = [];
+
+        checkboxes.forEach((checkbox) => {
+
+            if (checkbox.checked) {
+                selectGenres.push(checkbox.value);
+            } else {
+                const idx = selectGenres.indexOf(checkbox.value);
+                if (idx !== -1) {
+                    selectGenres.splice(idx, 1);
+                }
+            }
+
+        });
+
+        while (container.lastElementChild) {
+            container.removeChild(container.lastElementChild);
+        }
+
+        if (selectGenres.length <= 0) {
+
+            for (var i = 0; i < movies.movies.length; i++) {
+                let title = movies.movies[i].title;
+                let desc = movies.movies[i].description;
+    
+                let div = document.createElement("div");
+                div.innerHTML = `
+                    <h3>${title}<\h3><br>
+                    <p>${desc}<\p>
+                `;
+    
+                container.append(div);
+            }
+
+        } else {
+
+            for (var i = 0; i < movies.movies.length; i++) {
+
+                if (movies.movies[i].genres.some((item) => selectGenres.includes(item))) {
+                    let title = movies.movies[i].title;
+                    let desc = movies.movies[i].description;
+            
+                    let div = document.createElement("div");
+                    div.innerHTML = `
+                        <h3>${title}<\h3><br>
+                        <p>${desc}<\p>
+                    `;
+            
+                    container.append(div);
+                }
+
+            }
+        }
+    }
 
 }
 
@@ -48,7 +119,6 @@ function getLoadSearch() {
     
         for (var i = 0; i < movies.movies.length; i++) {
             let title = movies.movies[i].title;
-            console.log(title.includes(inputSearchName));
             if (title.includes(inputSearchName)) {
                 let title = movies.movies[i].title;
                 let desc = movies.movies[i].description;
