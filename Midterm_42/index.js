16876995531401f2f7eccadbcc643d9b62f50e72
Dylan <dbooth05@iastate.edu>
2024-02-28 10:44:38 -0600

    fix of script

--- Midterm_42/index.js
+++ Midterm_42/index.js
@@ -1,137 +1,91 @@
-window.onload = getLoadAll;
+window.onload = getLoadAll();
+
+function getFilters() {
+
+    var checkboxes = document.querySelectorAll('#genres input[type="checkbox"]');
+    var filteredList = [];
+
+    checkboxes.forEach((checkbox) => {
+        if (checkbox.checked) {
+            filteredList.push(checkbox.value);
+        } else {
+            const idx = filteredList.indexOf(checkbox.value);
+            if (idx !== -1) {
+                filteredList.splice(idx, 1);
+            }
+        }
+    });
+
+    return filteredList;
+}
 
 function getLoadAll() {
 
     fetch("./data.json")
-    .then(response => response.json())
-    .then(movies => loadAll(movies));
+        .then(response => response.json())
+        .then(movies => loadAll(movies));
 
     function loadAll(movies) {
         
         var container = document.getElementById("movie_list");
 
-        console.log("made it here");
-
-        while (container.lastElementChild) {
-            container.remove(container.lastElementChild);
-        }
-
-        for (var i = 0; i < movies.movies.length; i++) {
-            let title = movies.movies[i].title;
-            let desc = movies.movies[i].description;
-
-            let div = document.createElement("div");
-            div.innerHTML = `
-                <h3>${title}<\h3><br>
-                <p>${desc}<\p>
-            `;
-
-            container.append(div);
-        }
-
-    }
-}
-
-function getLoadFilter() {
-
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(movies => loadFilter(movies));
-
-    function loadFilter(movies) {
-
-        var container = document.getElementById("movie_list");
-
-        var checkboxes = document.querySelectorAll('#genres input[type="checkbox"]');
-        var selectGenres = [];
-
-        checkboxes.forEach((checkbox) => {
-
-            if (checkbox.checked) {
-                selectGenres.push(checkbox.value);
-            } else {
-                const idx = selectGenres.indexOf(checkbox.value);
-                if (idx !== -1) {
-                    selectGenres.splice(idx, 1);
-                }
-            }
-
-        });
-
+        filtered = getFilters();
+        
+        let searchName = document.forms["search_form"]["search"];
+        let inputSearchName = searchName.value;   
+            
         while (container.lastElementChild) {
             container.removeChild(container.lastElementChild);
         }
 
-        if (selectGenres.length <= 0) {
-
+        if (filtered.length <= 0) {
+        
             for (var i = 0; i < movies.movies.length; i++) {
                 let title = movies.movies[i].title;
-                let desc = movies.movies[i].description;
-    
-                let div = document.createElement("div");
-                div.innerHTML = `
-                    <h3>${title}<\h3><br>
-                    <p>${desc}<\p>
-                `;
-    
-                container.append(div);
-            }
-
-        } else {
-
-            for (var i = 0; i < movies.movies.length; i++) {
-
-                if (movies.movies[i].genres.some((item) => selectGenres.includes(item))) {
-                    let title = movies.movies[i].title;
+                if (title.includes(inputSearchName)) {
                     let desc = movies.movies[i].description;
             
                     let div = document.createElement("div");
                     div.innerHTML = `
-                        <h3>${title}<\h3><br>
-                        <p>${desc}<\p>
+                        <div id="data">
+                            <h3>${title}<\h3><br>
+                            <p>${desc}<\p>
+                        </div>
+                            <div id="get_info">
+                            <button type="button" onclick="">View More Info</button>
+                        </div>
                     `;
             
                     container.append(div);
                 }
+            }
+    
+        } else {
+            for (var i = 0; i < movies.movies.length; i++) {
+
+                if (movies.movies[i].genres.some((item) => filtered.includes(item))) {
+                    let title = movies.movies[i].title;
+                    if (title.includes(inputSearchName)) {
+                        let desc = movies.movies[i].description;
+                
+                        let div = document.createElement("div");
+                        div.innerHTML = `
+                        <div id="data">
+                            <h3>${title}<\h3> </br>
+                            <p>${desc}<\p>
+                        </div>
+                            <div id="get_info">
+                            <button type="button" onclick="">View More Info</button>
+                        </div>
+                        `;
+                
+                        container.append(div);
+                    }
+                }
 
             }
-        }
-    }
-
-}
-
-function getLoadSearch() {
-
-    fetch("./data.json")
-        .then(response => response.json())
-        .then(movies => loadSearch(movies));
-
-    function loadSearch(movies) {
-
-        var container = document.getElementById("movie_list");
-
-        let searchName = document.forms["search_form"]["search"];
-        let inputSearchName = searchName.value;
-
-        while (container.lastElementChild) {
-            container.removeChild(container.lastElementChild);
         }
     
-        for (var i = 0; i < movies.movies.length; i++) {
-            let title = movies.movies[i].title;
-            if (title.includes(inputSearchName)) {
-                let title = movies.movies[i].title;
-                let desc = movies.movies[i].description;
-        
-                let div = document.createElement("div");
-                div.innerHTML = `
-                    <h3>${title}<\h3><br>
-                    <p>${desc}<\p>
-                `;
-        
-                container.append(div);
-            }
-        }
     }
 
 }
